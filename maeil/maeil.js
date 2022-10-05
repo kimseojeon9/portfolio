55fe7e709291544214e408e369c73db8a8e40274
kimseojeong <114711730+kimseojeon9@users.noreply.github.com>
2022-10-05 04:32:48 +0000

    업로드

--- maeil/maeil.js
+++ maeil/maeil.js
@@ -1,23 +1,28 @@
 $(function () {
-    $(".slide").slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        dots: true,
-      });
 
-      $(".slide_brn").slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true
-      });
-
-      // $("selectBox").click(function(){
-      //   if($("selectList").css({"display": "none"}))
-      // });
-})
+  $(".slide").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+  });
   
+  $(".slide_brn").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
+  
+  $(".site").click(function () {
+    if ($(this).hasClass("on")) {
+      $(this).add(".selectList").removeClass("on");
+    } else {
+      $(this).add(".selectList").addClass("on");
+    }
+    return false;
+  });
+});
