43e6289168072e1e5239168dcf198882d78636bd
kimseojeong <114711730+kimseojeon9@users.noreply.github.com>
2022-10-06 07:19:30 +0000

    매일유업 업로드
    헤더

--- maeil/maeil.js
+++ maeil/maeil.js
@@ -1,5 +1,29 @@
 $(function () {
 
+  // 헤더 메뉴
+  var iw = window.innerWidth;
+  if(iw > 1024) {
+    $(".gnb .m").on("mouseenter focusin", function(){
+      $("ul", this).slideDown();
+      $(this).siblings("li").find("ul").hide();
+      $(this).addClass("on").siblings("li").removeClass("on");
+    }).on("mouseleave", function() {
+      $(".gnb ul").hide().add(".gnb .m").removeClass("on");            
+  });
+  } else {
+    $(".gnb .m").on("click", function(){
+      $("ul", this).slideDown();
+      $(this).siblings("li").find("ul").hide();
+      $(this).addClass("on").siblings("li").removeClass("on");
+      return false
+    });
+  }
+  $("*:not(.gnb .m)").on("focus", function(){
+    $(".gnb .m").hide().add(".gnb .m").removeClass("on");
+  });
+
+
+  // 비주얼 슬릭슬라이드
   $(".slide").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -7,7 +31,8 @@ $(function () {
     autoplaySpeed: 2000,
     dots: true,
   });
-  
+
+  // 브랜드 슬릭슬라이드
   $(".slide_brn").slick({
     dots: true,
     infinite: true,
@@ -16,7 +41,8 @@ $(function () {
     centerMode: true,
     variableWidth: true
   });
-  
+
+  // 패밀리 사이트
   $(".site").click(function () {
     if ($(this).hasClass("on")) {
       $(this).add(".selectList").removeClass("on");
@@ -25,4 +51,9 @@ $(function () {
     }
     return false;
   });
+  $("html").click(function(event){
+    $(".site").add(".selectList").removeClass("on");
+    event.stopPropagation();
+});
+
 });
