7f237db4fd28b4de4b1939d5fe628f030d400f68
kimseojeong <114711730+kimseojeon9@users.noreply.github.com>
2022-10-07 08:31:27 +0000

    헤더

--- maeil/maeil.js
+++ maeil/maeil.js
@@ -1,27 +1,23 @@
 $(function () {
 
   // 헤더 메뉴
-  var iw = window.innerWidth;
-  if(iw > 1024) {
-    $(".gnb .m").on("mouseenter focusin", function(){
-      $("ul", this).slideDown();
-      $(this).siblings("li").find("ul").hide();
-      $(this).addClass("on").siblings("li").removeClass("on");
-    }).on("mouseleave", function() {
-      $(".gnb ul").hide().add(".gnb .m").removeClass("on");            
-  });
-  } else {
-    $(".gnb .m").on("click", function(){
-      $("ul", this).slideDown();
-      $(this).siblings("li").find("ul").hide();
-      $(this).addClass("on").siblings("li").removeClass("on");
-      return false
-    });
-  }
-  $("*:not(.gnb .m)").on("focus", function(){
-    $(".gnb .m").hide().add(".gnb .m").removeClass("on");
-  });
+  $(".gnb").on("mouseenter focusin", function(){
+    $(".gnb>div>span").addClass("on").siblings().removeClass("on");
+});
 
+if ($(window).width()<1024) {
+    $("mMenu").click(function(){
+        $(".gnb").addClass("on");
+    });
+    $(".mClose").click(function(){
+        $(".gnb").removeClass("on")
+    });
+
+    $(".gnb>div>span").click(function(){
+        $(this).addClass("on").siblings().removeClass("on");
+        $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on")
+    });
+}
 
   // 비주얼 슬릭슬라이드
   $(".slide").slick({
