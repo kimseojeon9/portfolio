c6df4057b187a716febba6ff5a6c97208c3e822b
kimseojeong <114711730+kimseojeon9@users.noreply.github.com>
2022-10-18 08:00:16 +0000

    사이드바 수정

--- maeil/js/maeilsub.js
+++ maeil/js/maeilsub.js
@@ -64,6 +64,20 @@ $(function () {
         return false;
     });
 
+    // 탭 스크롤 이벤트
+    console.log($(".con2 .sec2 .tab").offset())
+
+    var tabScroll = $(".con2 .sec2 .tab").offset();
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= tabScroll.top) {
+            $(".con2 .sec2 .tab").css({"border-radius": "0"});
+            $(".con2 .sec2 .tab ul li").css({"border-radius": "0"});
+        } else {
+            $(".con2 .sec2 .tab").css({"border-radius": "30px"});
+            $(".con2 .sec2 .tab ul li").css({"border-radius": "30px"});
+        }
+    });
+
 });
 
 /* -------------------------------------------------- */
