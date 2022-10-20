de6286c096172ab9e018934299028a556aea1ddf
kimseojeong <114711730+kimseojeon9@users.noreply.github.com>
2022-10-20 08:28:21 +0000

    폼 업로드

---
+++ maeil/js/maeilform.js
@@ -0,0 +1,88 @@
+
+$(function () {
+    // 헤더
+    $(".gnb").on("mouseenter focusin", function () {
+        $(".gnb>div>span").on("focusin click", function () {
+            $(this).addClass("on").siblings().removeClass("on");
+            $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on");
+            $(".subGnb").addClass("on");
+        });
+    });
+
+    $("#sub06 li:last a").on("focusout", function () {
+        $(".subGnb").removeClass("on");
+    });
+
+    // 모바일 헤더
+    if ($(window).width() < 1024) {
+        $(".mMenu").click(function () {
+            $(".gnb").addClass("on");
+        });
+        $(".mClose").click(function () {
+            $(".gnb").removeClass("on");
+        });
+    }
+
+    // 패밀리 사이트
+    $(".site").click(function () {
+        if ($(this).hasClass("on")) {
+            $(this).add(".selectList").removeClass("on");
+        } else {
+            $(this).add(".selectList").addClass("on");
+        }
+        return false;
+    });
+    $("html").click(function (event) {
+        $(".site").add(".selectList").removeClass("on");
+        event.stopPropagation();
+    });
+
+    // 스크롤 이벤트
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 75) {
+            $(".topbt").addClass("on");
+        } else {
+            $(".topbt").removeClass("on");
+        }
+    });
+
+    // 탑 버튼
+    $(".topbt").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 500);
+    });
+
+    
+/* -------------------------------------------------- */
+
+    // step eq
+    $(".formBox .step ul li").click(function(){
+        var stepIndex = $(this).index();
+        $(this).addClass("on").siblings().removeClass("on");
+        $(".formBox .bwrap").eq(stepIndex).addClass("on").siblings().removeClass("on");
+        return false;
+    });
+
+    // button click
+    $(".formBox .box .content > div > ul li button").click(function(){
+        $(this).addClass("on").siblings().removeClass("on");
+        return false;
+    });
+
+});
+
+/* -------------------------------------------------- */
+
+// 리사이징
+$(window).resize(function () {
+    if ($(window).width() < 1024) {
+        $(".mMenu").click(function () {
+            $(".gnb").addClass("on");
+        });
+        $(".mClose").click(function () {
+            $(".gnb").removeClass("on");
+        });
+    }
+
+});
+
+
