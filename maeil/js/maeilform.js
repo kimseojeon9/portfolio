c8d2b0c0f68d5432eb9410dd1f7d4b188a25184b
kimseojeong <114711730+kimseojeon9@users.noreply.github.com>
2022-10-25 07:42:52 +0000

    매일유업 폼 업로드

--- maeil/js/maeilform.js
+++ maeil/js/maeilform.js
@@ -112,10 +112,15 @@ $(function () {
         $(".formBox .step ul li").eq(nextpage + 1).addClass("on").siblings().removeClass("on");
     });
 
-    $(".formBox .bwrap .bt_wrap > .prev").click(function () {
+    $(".formBox .bwrap.tres .bt_wrap > .prev").click(function () {
         var prevpage = $(this).index();
         $(".formBox .bwrap").eq(prevpage < 1).addClass("on").siblings().removeClass("on");
         $(".formBox .step ul li").eq(prevpage < 1).addClass("on").siblings().removeClass("on");
+    });
+    $(".formBox .bwrap.dos .bt_wrap > .prev").click(function () {
+        var prevpage = $(this).index();
+        $(".formBox .bwrap").eq(prevpage).addClass("on").siblings().removeClass("on");
+        $(".formBox .step ul li").eq(prevpage).addClass("on").siblings().removeClass("on");
     });
 
 
@@ -127,30 +132,7 @@ $(function () {
         $(".submit_pop").hide();
     });
 
-    
-    //체크박스 구현
-    // 전체 동의
-    $('#admitAll').on('click', function () {
-        var checked = $(this).is(':checked');
-        if (checked) {
-            $(this).closest('.admit_wrap').find('.check').prop('checked', true);
-        } else {
-            $(this).closest('.admit_wrap').find('.check').prop('checked', false);
-        }
-    });
 
-    //하나 선택시 전체 동의 해제
-    $('.admit_wrap .check').on('click', function () {
-        var chkGroup = $(this).closest('.admit_wrap').find('.admit');
-        var chkGroup_cnt = chkGroup.length;
-        checked_cnt = $('.admit .input-check:checked').length;
-
-        if (checked_cnt < chkGroup_cnt) {
-            $('#admitAll').prop('checked', false);
-        } else if (checked_cnt == chkGroup_cnt) {
-            $('#admitAll').prop('checked', true);
-        }
-    });
 
     /* ----------------------유효성 검사---------------------------- */
 
@@ -224,6 +206,30 @@ $(function () {
             return false;
         } else { // 서로 맞는경우
             $("#emaillog02").text("");
+        }
+    });
+
+    //체크박스 구현
+    // 전체 동의
+    $('#admitAll').on('click', function () {
+        var checked = $(this).is(':checked');
+        if (checked) {
+            $(this).closest('.admit_wrap').find('.check').prop('checked', true);
+        } else {
+            $(this).closest('.admit_wrap').find('.check').prop('checked', false);
+        }
+    });
+
+    //하나 선택시 전체 동의 해제
+    $('.admit_wrap .check').on('click', function () {
+        var chkGroup = $(this).closest('.admit_wrap').find('.admit');
+        var chkGroup_cnt = chkGroup.length;
+        checked_cnt = $('.admit input:checkbox:checked').length;
+
+        if (checked_cnt < chkGroup_cnt) {
+            $('#admitAll').prop('checked', false);
+        } else if (checked_cnt = 2) {
+            $('#admitAll').prop('checked', true);
         }
     });
 
