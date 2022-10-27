7d23db0d188049172ff1dbdd08259279a7c5d998
kimseojeong <114711730+kimseojeon9@users.noreply.github.com>
2022-10-27 07:23:28 +0000

    매일유업 폼 업로드
    클래스 명
    info, input 조정
    js 업데이트

--- maeil/js/maeilform.js
+++ maeil/js/maeilform.js
@@ -1,11 +1,10 @@
-// 유효성 검사
 var email = RegExp(/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i);
 var num = RegExp(/^(010|011|016|017|018|019)-[0-9]{3,4}-[0-9]{4}$/i);
 
-//콤마
 function addComma(data) {
     return data.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+
 
 $(function () {
     // 헤더
@@ -60,7 +59,7 @@ $(function () {
     });
 
 
-    /* ------------------------커스텀-------------------------- */
+    /* ------------------------기본 커스텀-------------------------- */
 
     // step eq
     $(".formBox .step ul li").click(function () {
@@ -71,10 +70,10 @@ $(function () {
     });
 
     // button click event
-    $(".formBox .box .content > div > ul li button").click(function () {
-        $(this).addClass("on").parent().siblings().children().removeClass("on");
+    $(".formBox .box .content > div > ul li button").click(function (e) {
+        $(this).addClass("on").parent().siblings().find('button').removeClass("on");
+        e.preventDefault();
     });
-
     $(".amount #number").on("click mousedown keydown", function () {
         $(this).addClass("on").parent().siblings().children().removeClass("on");
     });
@@ -84,26 +83,6 @@ $(function () {
         $(this).val($(this).val().replace(/[^0-9]/g, "")); //숫자만 입력
         $(this).val(addComma($(this).val().replace(/[^0-9]/g, ""))); //콤마
     });
-
-
-    // 버튼 클릭시 탭 전환
-    // 2 후원자 정보 구분
-    $(".bwrap.dos .content > div:first > ul li button").click(function () {
-        $(this).addClass("on").parent().siblings().children().removeClass("on");
-        $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on");
-    });
-    // 3 결제수단 구분
-    $(".bwrap.tres .content > div:first > ul li button").click(function () {
-        $(this).addClass("on").parent().siblings().children().removeClass("on");
-        $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on");
-    });
-    // 3 예금주 구분
-    $(".bwrap.tres .content > div:nth-of-type(2) > div:nth-of-type(4) ul li button").click(function () {
-        $(this).addClass("on").parent().siblings().children().removeClass("on");
-        $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on");
-    });
-
-
 
     // next prev
     $(".formBox .bwrap .bt_wrap > .next").click(function () {
@@ -123,6 +102,81 @@ $(function () {
         $(".formBox .step ul li").eq(prevpage).addClass("on").siblings().removeClass("on");
     });
 
+    // 버튼 클릭시 탭 전환
+    // 2 후원자 정보 구분
+    $(".bwrap.dos .content > div:first > ul li button").click(function () {
+        $(this).addClass("on").parent().siblings().children().removeClass("on");
+        $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on");
+    });
+    // 3 결제수단 구분
+    $(".bwrap.tres .content > div:first > ul li button").click(function () {
+        $(this).addClass("on").parent().siblings().children().removeClass("on");
+        $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on");
+    });
+    // 3 예금주 구분
+    $(".bwrap.tres .content > div:nth-of-type(2) > div:nth-of-type(4) ul li button").click(function () {
+        $(this).addClass("on").parent().siblings().children().removeClass("on");
+        $("#" + $(this).data("id")).addClass("on").siblings().removeClass("on");
+    });
+
+
+    /* ------------------------단계별-------------------------- */
+
+    //0 빈값 체크
+    // $("bt_wrap .next").on("click", function(){
+    //     var empty = $('input:value').attr()
+    //     var empty2 = $('button:value').attr()
+        
+    // });
+
+    //1 일시후원 클릭시 출금일 없애기
+    $(".cycle button#keep").click(function(){
+        $(".wdate").css({"display": "flex"});
+    });
+    $(".cycle button#nokeep").click(function(){
+        $(".wdate").css({"display": "none"});
+    });
+
+    //1 input에 5천원 이하 입력시
+    $("#number").blur(function(){
+        var avg = $("#number").value;
+        var comment = `한 어르신께 일주일간 안부를 묻기 위해 약 5,000원 이상의 후원금이 필요합니다.`;
+        if (avg < 5000) {
+        }
+        alert(comment);
+    });
+
+    //1 버튼 값 span에 넣기
+    // $(".bwrap.uno button").on('click', function(){
+    //     var cycle = $('.cycle button').on('click', function(e) {
+    //         console.log(e.target.value);
+    //         $('.cycle button').attr(e.target.value);
+    //       });
+    //     var amount = $('.amount button').on('click', function(e) {
+    //         console.log(e.target.value);
+    //         $('.amount button').attr(e.target.value);
+    //       });
+    //     $("#cycle").text(`${cycle}`);
+    //     $("#amount").text(`${amount}`);
+    // });
+    
+    //3 은행
+    $.ajax({
+        type: "GET",
+        url: "/maeil/js/bank.json",
+        dataType: "json",
+        success: function(data) {
+            var elem = ""
+            console.log(data)
+            $.each(data, function (index, obj) {
+                elem += `<option>${obj.name}</option>`
+            });
+            $("#bank").append(elem);
+        },
+        error: function(xhr) {
+            console.log(`${xhr.status}/${xhr.errorText}`)
+        }
+    });
 
     //3 popup
     $(".formBox .bwrap .bt_wrap > .submit").click(function () {
@@ -171,7 +225,6 @@ $(function () {
             $("#emaillog01").text("");
         }
     });
-
     // --------------------------
     $("#num02").blur(function () {
         //전화번호 공백 검사
@@ -208,6 +261,8 @@ $(function () {
             $("#emaillog02").text("");
         }
     });
+
+    /* ----------------------체크박스------------------------ */
 
     //체크박스 구현
     // 전체 동의
@@ -251,4 +306,6 @@ $(window).resize(function () {
 
 });
 
+
+/* -------------------------------------------------- */
 
