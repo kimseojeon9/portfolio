714c87e5dbc5b9b256eeb4155787f7ee170fc605
kimseojeong <114711730+kimseojeon9@users.noreply.github.com>
2022-11-15 05:35:08 +0000

    청주시문화산업진흥재단

--- cjculture/js/cjculture.js
+++ cjculture/js/cjculture.js
@@ -41,3 +41,48 @@ var swiper = new Swiper(".space_slide", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+$(document).ready(function () {
+  // header
+  $('.snb, .snbbg').slideUp(0);
+  $('.gnb li').hover(function () {
+    $('.snb, .snbbg').stop().slideDown('500');
+  }, function () {
+    $('.snb, .snbbg').stop().slideUp('500');
+  });
+
+  $('.mMenu').on('click', function () {
+    $('.snb, .snbbg').show("fast");
+  });
+
+  // sec02 
+  $('.title li').on('click', function () {
+    $(this).addClass('on').siblings().removeClass('on');
+  });
+
+  // banners 
+  $('banners div:gt(0)').hide();
+  var totalNum = $(".banners>div").length;
+  var currentNum = 1;
+  $(".page_n>span:first").text(currentNum);
+  $(".page_n>span:last").text(totalNum);
+
+  $(".next_b").click(function () {
+    currentNum++;
+    if (currentNum > totalNum) {
+      currentNum = 1;
+    }
+    $(".banners div:first").insertAfter(".banners div:last");
+    $(".page_n>span:first").text(currentNum);
+  });
+
+  $(".prev_b").click(function () {
+    currentNum--;
+    if (currentNum < 1) {
+      currentNum = totalNum;
+    }
+    $(".banners div:last").insertBefore(".banners div:first");
+    $(".page_n>span:first").text(currentNum);
+  });
+
+});
